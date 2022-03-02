baa651de861bb19d135a17bd582410e01f709c89
Saitaruntulabandula <saitaruntulabandula@gmail.com>
2022-03-02 21:15:00 +0530

    EmployeeAttendance

--- EmployeeWage/src/com/bridgelabz/EmployeeWage.java
+++ EmployeeWage/src/com/bridgelabz/EmployeeWage.java
@@ -3,7 +3,16 @@ package com.bridgelabz;
 public class EmployeeWage {
 
 	public static void main(String[] args) {
-		System.out.println("Welcome to Employee Wage Program");
+		int isPresent=1;
+	     double value=Math.floor(Math.random()*10)%2;
+	     if(value==isPresent)
+	     {
+	    	 System.out.println("Emp is Present");
+	     }
+	     else
+	     {
+	    	 System.out.println("Emp is Absent");
+	     }
 	}
 
 }
