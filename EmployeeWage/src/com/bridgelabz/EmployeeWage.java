41b5d1f2e21b21a23ee0de30bfcd819edb18d3e7
Saitaruntulabandula <saitaruntulabandula@gmail.com>
2022-03-02 22:57:24 +0530

    EmployeeDailyWage

--- EmployeeWage/src/com/bridgelabz/EmployeeWage.java
+++ EmployeeWage/src/com/bridgelabz/EmployeeWage.java
@@ -1,18 +1,19 @@
 package com.bridgelabz;
 
 public class EmployeeWage {
-
 	public static void main(String[] args) {
+		int fullDayWork=8;
+		int wagePerHr=20;
 		int isPresent=1;
-	     double value=Math.floor(Math.random()*10)%2;
-	     if(value==isPresent)
-	     {
-	    	 System.out.println("Emp is Present");
-	     }
-	     else
-	     {
+	    double value=Math.floor(Math.random()*10)%2;
+	    if(value==isPresent)
+	    {
+	    	System.out.println("Emp is Present");
+	    	System.out.println("Employee wage =" +fullDayWork*wagePerHr);
+	    }
+	    else
+	    {
 	    	 System.out.println("Emp is Absent");
-	     }
+	    }
 	}
-
 }
