6f7c447cdab49ecad4734579a689af84344db265
Saitaruntulabandula <saitaruntulabandula@gmail.com>
2022-03-03 09:01:21 +0530

    MonthlyWages

--- EmployeeWage/src/com/bridgelabz/EmployeeWage.java
+++ EmployeeWage/src/com/bridgelabz/EmployeeWage.java
@@ -4,16 +4,17 @@ public class EmployeeWage {
 	public static void main(String[] args) {
 		int wagePerHr=20;
 		int isPresent=1;
-		int partTimeHours=8;
-	    double value=Math.floor(Math.random()*10)%2;
-	    if(value==isPresent)
-	    {
+		int workingHours=8;
+		int workingDays=20;
+		double value=Math.floor(Math.random()*10)%2;
+	     if(value==isPresent)
+	     {
 	    	 System.out.println("Emp is Present");
-	    	 System.out.println("Wage of the part time Employee is = " +wagePerHr*partTimeHours);
-	    }
-	    else
-	    {
+	    	 System.out.println("Wage of the Full Time Employee is = " +workingDays*workingHours*wagePerHr);
+	     }
+	     else
+	     {
 	    	 System.out.println("Emp is Absent");
-	    }
+	     }
 	}
 }
